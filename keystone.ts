a801f9e7e9701a9ac0aae8f05d8f36b7d648f260
Greg Leeper <gregleeper@Greg-Leeper-Hugoton-USD-210.local>
2021-10-25 20:19:55 -0500

    en variable change

--- keystone.ts
+++ keystone.ts
@@ -29,7 +29,7 @@ export default withAuth(
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
       provider: 'postgresql',
-      url: process.env.DATABASE_PROVIDER_URL,
+      url: process.env.DATABASE_URL,
     },
     server: {
       cors: {
